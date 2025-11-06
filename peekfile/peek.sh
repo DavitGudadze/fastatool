ab71065d05530e3be1ac3f1bdb29b3910b77e169
Davit Gudadze <davitgudadze@Davits-MacBook-Pro.local>
2025-11-06 02:55:43 +0100

    It´s raining raining

--- peekfile/peek.sh
+++ peekfile/peek.sh
@@ -3,7 +3,19 @@
 file="$1"
 n="$2"
 
-head -n $n "$file"
-echo "..."
-tail -n $n "$file"
+if [[ -z "$n" ]]; then
+  n=3
+fi
+
+lines=$(wc -l < "$file")
+
+if [[ $lines -le $((2 * n)) ]]; then 
+  echo "File has less or equal numbers , then requested"
+  cat "$file"
+else 
+  echo "Warning: file has more lines, then requested."
+  head -n "$n" "$file"
+  echo "..."
+  tail -n "$n" "$file"
+fi
 
